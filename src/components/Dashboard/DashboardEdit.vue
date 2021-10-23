6e668bf256abc6cac0d223ad8d6ffef1454a82e2
Nick <50417170+kanishev@users.noreply.github.com>
2021-10-23 23:02:02 +0330

    create detaild popup

---
+++ src/components/Dashboard/DashboardEdit.vue
@@ -0,0 +1,48 @@
+<template>
+  <details-popup title="New Board" ref="popup">
+    <template v-slot:content>
+      <form>
+        <h4>Form Title</h4>
+        <input
+          name="boardName"
+          type="text"
+          class="form-control my-1"
+          data-vv-as="Board Name"
+          placeholder="Enter your board name"
+        />
+        <textarea
+          name="boardDescription"
+          rows="2"
+          class="form-control my-1"
+          data-vv-as="Board Description"
+          placeholder="Enter your board description"
+        ></textarea>
+        <button class="btn btn-sm btn-app mt-2" @click.prevent="saveBoard">
+          Save Board
+        </button>
+      </form>
+    </template>
+  </details-popup>
+</template>
+
+<script>
+import DetailsPopup from "../Details/DetailsPopup.vue";
+
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  methods: {
+    saveBoard() {
+      console.log("save board");
+    },
+  },
+  components: {
+    DetailsPopup,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Details/DetailsPopup.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-btn color="primary" dark @click.stop="dialog = true">
+      {{ title }}
+    </v-btn>
+
+    <v-dialog v-model="dialog" max-width="590">
+      <v-card>
+        <slot name="content"></slot>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="green darken-1" text @click="dialog = false">
+            Disagree
+          </v-btn>
+
+          <v-btn color="green darken-1" text @click="dialog = false">
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  methods: {
+    open() {
+      this.$refs.details.setAttribute("open", "");
+    },
+    close() {
+      this.$refs.details.removeAttribute("open");
+    },
+  },
+};
+</script>
+
+<style></style>

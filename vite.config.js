7de46e9aa43deaaebfce0c195c036f889f90bd44
Nick <50417170+kanishev@users.noreply.github.com>
2024-02-22 21:22:19 +0300

    vite vuetify resolver

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue2'
+import { defineConfig } from 'vite';
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+
+import vue from '@vitejs/plugin-vue2';
+
 
 const path = require("path");
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        // Vuetify
+        VuetifyResolver()
+      ]
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

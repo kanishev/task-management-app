834ac025775fd2c317219167a545b7d01a6ebd6b
Nick <50417170+kanishev@users.noreply.github.com>
2024-03-26 18:15:27 +0300

    eslint: install vite-plugin-eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import createVuePlugin from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()]
+  plugins: [createVuePlugin(), eslintPlugin()]
 })
